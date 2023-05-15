02a7cd553a851fdd53b5629fab7e707ab9db58ee
Lubor Pacak <pacaklu@seznam.cz>
2023-05-15 22:41:14 +0200

    api added

---
+++ api/app.py
@@ -0,0 +1,21 @@
+"""API for LGBMINFEROR class."""
+from fastapi import FastAPI
+
+from sreality_anomaly_detector.configs import inference_model_config
+from sreality_anomaly_detector.lgbm_inferor import LGBMModelInferor
+
+model = LGBMModelInferor(inference_model_config)
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    """Intro message."""
+    return {"Welcome to default endpoint for flat price prediction."}
+
+
+@app.post("/predict")
+def predict(input_data):
+    """Predict method of the model class."""
+    return model.predict(input_data)
