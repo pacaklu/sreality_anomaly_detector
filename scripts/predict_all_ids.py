dc051f33de8ae031a66e378a64cc4941a917c171
Lubor Pacak <pacaklu@seznam.cz>
2023-06-25 23:04:03 +0200

    sth

--- scripts/predict_all_ids.py
+++ scripts/predict_all_ids.py
@@ -4,9 +4,10 @@ import requests
 from sreality_anomaly_detector.configs import prediction_config
 
 if __name__ == "__main__":
-    data = pd.read_csv(prediction_config["data_path"])
-    flat_ids_to_test = data["ID"].tolist()
+    #data = pd.read_csv(prediction_config["data_path"])
+    #flat_ids_to_test = data["ID"].tolist()
 
-    for flat_id in flat_ids_to_test:
-        API_ENDPOINT = prediction_config["api_url"] + str(flat_id)
-        r = requests.post(url=API_ENDPOINT)
+    #for flat_id in flat_ids_to_test:
+    #    API_ENDPOINT = prediction_config["api_url"] + str(flat_id)
+    #    r = requests.post(url=API_ENDPOINT)
+    requests.post(url='http://localhost:8000/predict?input_data=3775812684')
