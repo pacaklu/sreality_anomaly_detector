8cdd4454177221c3d6c95ff18700443598e79394
Lubor Pacak <pacaklu@seznam.cz>
2023-06-25 23:52:22 +0200

    id to ID change

--- scripts/predict_all_ids.py
+++ scripts/predict_all_ids.py
@@ -12,5 +12,6 @@ if __name__ == "__main__":
     for flat_id in flat_ids_to_test:
         API_ENDPOINT = prediction_config["api_url"] + str(flat_id)
         r = requests.post(url=API_ENDPOINT)
+        logging.info(type(r))
         logging.info(r.json())
         #logging.INFO(f'prediction for {flat_id} is {r.json()}')
