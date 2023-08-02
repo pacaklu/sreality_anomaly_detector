8b73266711c8fc53bf1c44484e376a5675ad0d2e
Lubor Pacak <pacaklu@seznam.cz>
2023-08-03 00:25:02 +0200

    final changes

--- scripts/predict_all_ids.py
+++ scripts/predict_all_ids.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         logging.warning(f'Making prediction for flat id {flat_id}')
         api_url = prediction_config["api_url"] + str(flat_id)
         try:
-            r = requests.post(url=api_url)
+            r = requests.post(url=api_url, timeout = 5)
             extracted_data = r.json()
 
             flat_ids.append(flat_id)
