f3f829e64391b48428b1b218868d19c7321ffd94
Lubor Pacak <pacaklu@seznam.cz>
2024-01-07 21:43:06 +0100

    sth

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+USE_API = False
+
 cd /home/ec2-user/sreality_anomaly_detector
 git pull
 
@@ -8,14 +10,18 @@ docker rm -vf $(docker ps -aq)
 # Delete all images
 docker rmi -f $(docker images -aq)
 
-#docker build -f dockerfiles/scrape.Dockerfile  . -t scraper
-#docker run -v /home/ec2-user/data/:/data/ scraper
+docker build -f dockerfiles/scrape.Dockerfile  . -t scraper
+docker run -v /home/ec2-user/data/:/data/ scraper
 
-#docker build -f dockerfiles/training.Dockerfile . -t trainer_image
-#docker run -v /home/ec2-user/data/:/data -v /home/ec2-user/models/:/models trainer_image
+docker build -f dockerfiles/training.Dockerfile . -t trainer_image
+docker run -v /home/ec2-user/data/:/data -v /home/ec2-user/models/:/models trainer_image
 
-docker build -f dockerfiles/api.Dockerfile . -t api
-docker run -v /home/ec2-user/models/:/models -d -p 8000:8000 api
+if [$USE_API = True] ;
+then:
+  docker build -f dockerfiles/api.Dockerfile . -t api
+  docker run -v /home/ec2-user/models/:/models -d -p 8000:8000 api
+fi
+
 # Can be tested with following command
 #curl -X POST http://localhost:8000/predict?input_data=4065768524
 
